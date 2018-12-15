040a814f97a2dd276f7fdf11b77938c7e80249b8
Jonathan Gaghan <jpgaghan@gmail.com>
2018-12-14 19:12:40 -0500

    heroku

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import Footer from "./components/Footer";
 import Friends from "./friends.json";
 import Card from "./components/FriendCard";
 import _ from "lodash";
-// import Image from "./components/Image"
-// import Container from ".components/Container"
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
@@ -19,8 +17,8 @@ class App extends Component {
   };
 
   correctGuess = (clickedid, Friends, score, topscore) => {
-    Friends = Friends.map(({id, clickstatus, image}) => {if(clickedid === id) { return {id, clickstatus: true, image}} else { return {id, clickstatus, image}}});
-    switch(true) {
+    Friends = Friends.map(({ id, clickstatus, image }) => { if (clickedid === id) { return { id, clickstatus: true, image } } else { return { id, clickstatus, image } } });
+    switch (true) {
       case score === 15:
         this.setState({
           Friends,
@@ -30,27 +28,25 @@ class App extends Component {
         })
         break;
       case score >= topscore:
-      console.log(score, topscore)
         this.setState({
           Friends,
-          score: score+=1,
-          topscore: topscore+=1,
+          score: score += 1,
+          topscore: topscore += 1,
           headline: "Click Away"
         })
         break;
       default:
         this.setState({
           Friends,
-          score: score+=1,
+          score: score += 1,
           topscore,
           headline: "Click Away"
         })
+    };
   };
-};
 
   incorrectGuess = (Friends, score, topscore) => {
-    Friends = Friends.map(({id, clickstatus, image}) => {if(clickstatus) { return {id, clickstatus: false, image}} else { return {id, clickstatus, image}}});
-    console.log(Friends)
+    Friends = Friends.map(({ id, clickstatus, image }) => { if (clickstatus) { return { id, clickstatus: false, image } } else { return { id, clickstatus, image } } });
     this.setState({
       Friends,
       score: 0,
@@ -60,23 +56,24 @@ class App extends Component {
   };
 
   cardClick = (id, status) => {
-    console.log(status);
-    let {Friends, score, topscore} = this.state;
-    status ?  this.incorrectGuess(Friends, score, topscore) : this.correctGuess(id, Friends, score, topscore)
+    let { Friends, score, topscore } = this.state;
+    status ? this.incorrectGuess(Friends, score, topscore) : this.correctGuess(id, Friends, score, topscore)
   };
 
   render() {
-    return(
+    return (
       <Wrapper>
-        <Header score={this.state.score} topscore={this.state.topscore}/>
-        <Jumbotron headline={this.state.headline}/>
-          <div class = "container">
-            {_.shuffle(this.state.Friends).map(({id, image, clickstatus}) => <Card id={id} image={image} clickstatus ={clickstatus} onClick={() => this.cardClick(id, clickstatus)}/>)}
-          </div>
+        <Header score={this.state.score} topscore={this.state.topscore} />
+        <Jumbotron headline={this.state.headline} />
+        <div class="container">
+          {_.shuffle(this.state.Friends).map(({ id, image, clickstatus }) =>
+            <Card id={id} image={image} clickstatus={clickstatus} onClick={() =>
+              this.cardClick(id, clickstatus)} />)}
+        </div>
         <Footer />
       </Wrapper>
-      )
-  } 
+    )
+  }
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = props => (
     <header>
       <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <h1 class="navbar-brand text-white mx-auto align-middle">Fixed navbar</h1>
+        <h2 class="navbar-brand text-white mx-auto align-middle">Clicky Game</h2>
         <p class = "text-white mr-1">Current Score: {props.score}</p>
         <p class = "text-white">Top Score: {props.topscore}</p>
       </nav>
